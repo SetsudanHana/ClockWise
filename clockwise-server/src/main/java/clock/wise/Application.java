a06191194cb381b2e275de45e260d4ce08738e1d
Mateusz Wręczycki <setsudan.hana@gmail.com>
2016-04-10 23:07:11 +0200

    add logger to Application class

--- clockwise-server/src/main/java/clock/wise/Application.java
+++ clockwise-server/src/main/java/clock/wise/Application.java
@@ -1,5 +1,7 @@
-package hello;
+package clock.wise;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,15 +11,17 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        log.info("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            log.info(beanName);
         }
     }
 
