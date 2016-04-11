8a2adbaa18d25808fc03a040c2a2f6a3f93543b8
Mateusz Wręczycki <setsudan.hana@gmail.com>
2016-04-11 21:59:24 +0200

    add missing checkAndCreateSuperAdmin invocation

--- clockwise-server/src/main/java/clock/wise/configuration/WebSecurityConfig.java
+++ clockwise-server/src/main/java/clock/wise/configuration/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService()).passwordEncoder(encoder);
+        checkAndCreateSuperAdmin();
     }
 
     @Override
