1746a4a3c5a4c252eea622dc9a3d5182c8790882
Mateusz Wręczycki <setsudan.hana@gmail.com>
2016-04-13 00:44:34 +0200

    make project compile again

--- clockwise-server/src/main/java/clock/wise/service/UserService.java
+++ clockwise-server/src/main/java/clock/wise/service/UserService.java
@@ -1,8 +1,6 @@
 package clock.wise.service;
 
 import clock.wise.dtos.UserDto;
-import clock.wise.model.User;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Created by Gniewko on 2016-04-12.
@@ -10,8 +8,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public interface UserService
 {
     UserDto addUser(final UserDto userDto);
-
-    UserDto findByUsername(final String username) throws UsernameNotFoundException;
 
     UserDto getUser( long id);
 
--- clockwise-server/src/main/java/clock/wise/service/impl/UserServiceImpl.java
+++ clockwise-server/src/main/java/clock/wise/service/impl/UserServiceImpl.java
@@ -5,16 +5,19 @@ import clock.wise.dao.UserDao;
 import clock.wise.dtos.UserDto;
 import clock.wise.model.User;
 import clock.wise.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService
 {
-    @Autowired
-    private UserModelConverter userModelConverter;
+    private ModelMapper modelMapper = new ModelMapper();
+
+    {
+        modelMapper.addConverter(new UserModelConverter());
+    }
     @Autowired
     private UserDao userDao;
 
@@ -22,20 +25,8 @@ public class UserServiceImpl implements UserService
     @Transactional
     public UserDto addUser( UserDto userDto )
     {
-        User user = userModelConverter.convertDtoToEntity( userDto );
-        return userModelConverter.convert( userDao.save( user ) );
-    }
-
-    @Override
-    @Transactional
-    public UserDto findByUsername( String username ) throws UsernameNotFoundException
-    {
-        UserDto userDto = userModelConverter.convert( userDao.findOneByUsername( username ) );
-        if ( userDto == null )
-        {
-            throw new UsernameNotFoundException( "Username: " + username + " not found" );
-        }
-        return userDto;
+        User user = modelMapper.map(userDto, User.class);
+        return modelMapper.map(userDao.save(user), UserDto.class);
     }
 
     @Override
