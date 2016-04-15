94b0f5aeeaaccd2cdc1e7eb52f064f0e4c1b2971
Gniewko <ggardas19@gmail.com>
2016-04-15 22:02:10 +0200

    drobna poprawka do dao

--- clockwise-server/src/main/java/clock/wise/dao/impl/UserDaoImpl.java
+++ clockwise-server/src/main/java/clock/wise/dao/impl/UserDaoImpl.java
@@ -1,14 +1,9 @@
 package clock.wise.dao.impl;
 
 import clock.wise.dao.UserDao;
-import clock.wise.model.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public abstract class UserDaoImpl extends CrudRepositoryImpl< User, Long > implements UserDao
+public abstract class UserDaoImpl implements UserDao
 {
-    public UserDaoImpl()
-    {
-        super( User.class );
-    }
 }
