4f8bdf16b0eb3f175addb4e5b4cac6abff0c4c67
Armageddonw <m.wylezol1@op.pl>
2016-06-13 21:17:08 +0200

    Revert "Revert "Company label updated""
    This reverts commit 564cd2d74d81c36d74a7fcd0718a565760ae34cf.

--- clockwise-desktop/clockwise-desktop/Authentication.h
+++ clockwise-desktop/clockwise-desktop/Authentication.h
@@ -23,6 +23,7 @@ public:
 
 private:
 	User* loadUser(const std::wstring& Username);
+	std::wstring getCompany(int CompanyId);
 
 	ServiceCommunicator& communicator;
 	std::unique_ptr<User> LoggedUser;
